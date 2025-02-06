5fcc4b8e214eb3fc8b243092721c7b1453aa32f9
slashbade <slashbladevitali@gmail.com>
2025-02-07 03:58:22 +0800

    finish most of them

--- LeanMinicourse/L1_Introduction/Calculation.lean
+++ LeanMinicourse/L1_Introduction/Calculation.lean
@@ -1,24 +1,42 @@
-import Mathlib
+import Mathlib.Analysis.RCLike.Basic
+import Mathlib.Data.Complex.Trigonometric
 import ReaperTac
+import Paperproof
 
 open Real
+
+/- Directly give the type of the proof -/
+example (a : ℝ) : a = a := rfl
 
 /- Directly give the type of the proof -/
 example (a b c : ℝ) : a * b * c = a * (b * c) :=
   mul_assoc a b c
 
+/- Construct the proof once is efficient, but is ugly
+and non-human -/
+example (a b c : ℝ) : a * b * c = b * c * a :=
+  (mul_assoc a b c).trans (mul_comm a (b * c))
+
 /- Most of time it is challenging to directy give the proof,
-so we choose to prove it interactively and step-by-step. This
-is a tactic mode. -/
+so we choose to prove it interactively and step-by-step.
+The keyword `by` will allow you enter tactic mode.
+`exact` is a simple tactic that finishes the proof.
+-/
 example (a b c : ℝ) : a * b * c = a * (b * c) := by
   exact mul_assoc a b c
 
-/- We can also use the `rewrite` tactic to rewrite the goal -/
+example (a : ℝ) : a = a := by
+  rfl
+
+/- We can also use the `rewrite` tactic to rewrite the goal
+You can rewrite either iff statements or equalities.
+ -/
 example (a b c : ℝ) : a * b * c = b * c * a := by
   rewrite [mul_assoc]
   rewrite [mul_comm]
   exact rfl -- rfl is a tactic that shows `x = x`, a reflexive proof
 
+/- TODEMO -/
 /- `rw` is a tactic combining `rewrite` and `rfl`, it tries to
 apply `rfl` immediately after `rewrite` -/
 example (a b c : ℝ) : a * b * c = b * c * a := by
@@ -26,8 +44,11 @@ example (a b c : ℝ) : a * b * c = b * c * a := by
   rw [mul_comm]
   -- rw [mul_comm, mul_assoc]
 
-/- A `calc` block allows you to proof an inequality step by step. -/
-example : (a + b) * (a + b) = a * a + 2 * (a * b) + b * b :=
+
+/- TODEMO -/
+/- A `calc` block allows you to proof an equality
+or inequality step by step. -/
+example (a b : ℝ) : (a + b) * (a + b) = a * a + 2 * (a * b) + b * b :=
   calc
     (a + b) * (a + b) = a * a + b * a + (a * b + b * b) := by
       rw [mul_add, add_mul, add_mul]
@@ -36,43 +57,56 @@ example : (a + b) * (a + b) = a * a + 2 * (a * b) + b * b :=
     _ = a * a + 2 * (a * b) + b * b := by
       rw [mul_comm b a, ← two_mul]
 
-/- `ring` is a tactic that can solve equations involving rings -/
+/- `ring` is a tactic that can solve equations involving rings
+First fact about ℝ, it is a ring. -/
 example : (a + b) * (a + b) = a * a + 2 * (a * b) + b * b := by
   ring
 
+-- #leansearch "Real.tan is equal to sin div by cos"
+#check tan_eq_sin_div_cos
+
+/- TODO -/
+lemma tan_mul_eq_sin_mul_div_cos_mul {α β : ℝ} : tan α * tan β = (sin α * sin β) / (cos α * cos β) := by
+  calc
+      tan α * tan β = (sin α / cos α) * (sin β / cos β) := by
+        rw [tan_eq_sin_div_cos, tan_eq_sin_div_cos]
+      _ = (sin α * sin β) / (cos α * cos β) := by
+        rw [mul_comm_div, div_div, mul_div, mul_comm (cos α)]
+
+/- TODEMO -/
 /- Here is a harder theorem, and an interesting property for real number. -/
 lemma div_eq_of_eq_mul₀ {a b c : ℝ} (h : c ≠ 0) : a / b = c → a = c * b := by
   intro h₁
   rw [← h₁] at h
   rw [div_ne_zero_iff] at h
   have := h.right
-  rw [(div_eq_iff this)] at h₁
+  rw [div_eq_iff this] at h₁
   exact h₁
 
-/-
-1. introduce rw
-3. introduce ring
-4. introduce have
--/
 
-#leansearch "Real.tan is equal to sin div by cos"
+/- `field_simp` is a carefully crafted tactic to solve problem on field. -/
+lemma div_eq_of_eq_mul₀' {a b c : ℝ} (h : b ≠ 0) : a / b = c → a = c * b := by
+  intro h₁
+  field_simp at h₁
+  exact h₁
+
+#leansearch "cos (a+b)"
+-- #leansearch "cos (a-b)"
 
 -- #herald "Real if a / b = c then a = b * c."
 /-- 2024 Gaokao Xinkebiao I - 4:  -/
 theorem gaokao_triangle (α β m : ℝ) (h₁ : cos (α + β) = m) (h₂ : tan α * tan β = 2) :
   cos (α - β) = -3 * m := by
-  have : tan α * tan β = (sin α * sin β) / (cos α * cos β) := by
-    calc
-      tan α * tan β = (sin α / cos α) * (sin β / cos β) := by
-        rw [tan_eq_sin_div_cos, tan_eq_sin_div_cos]
-      _ = (sin α * sin β) / (cos α * cos β) := by
-        rw [mul_comm_div, div_div, mul_div, mul_comm (cos α)]
-  rw [this] at h₂
-  apply div_eq_of_eq_mul₀ at h₂
+  /- We can use a `have` block to prove some lemmas -/
+  have : sin α * sin β = 2 * cos α * cos β := by
+    rw [tan_mul_eq_sin_mul_div_cos_mul] at h₂
+    rw [mul_assoc 2]
+    apply div_eq_of_eq_mul₀
+    simp
+    exact h₂
   rw [cos_add] at h₁
-  rw [h₂] at h₁
+  rw [this] at h₁
   rw [cos_sub]
-  rw [h₂]
+  rw [this]
   rw [← h₁]
   ring
-  simp
--- LeanMinicourse/L1_Introduction/Syntax.lean
+++ LeanMinicourse/L1_Introduction/Syntax.lean
@@ -1,4 +1,4 @@
-import Mathlib
+import Mathlib.Analysis.RCLike.Basic
 import ReaperTac
 
 open Nat
@@ -7,100 +7,118 @@ open Nat
 
 A framework for formalizing mathematics and logic reasoning.
 It is based on the idea that each value has a type.
-
 -/
--- Display the type of your value.
+
+/-
+## Type and Value
+To display the type of your expression, use `#check`.
+To evaluate your expression, use `#eval`
+When you input the value of an expression, Lean will try
+to infer the type of the value.
+-/
 #check 2 + 2
+#check 2 + 2.0
 #eval 2 + 2
 
-#check ℕ
-
 #check 2 / 3
-#eval 2 / 3
-#check 2 - 3 -- Something went wrong here.
-#eval 2 - 3
+#eval  2 / 3
+#check 2 - 3
+#eval  2 - 3
 
--- Explicitly declare the type of your value.
+/- You can also explicitly declare the type of your value. -/
 #check (2 : ℚ)
-#eval (2 / 3 : ℚ)
-#eval (2 - 3 : ℤ)
+#eval  (2 / 3 : ℚ)
+#eval  (2 - 3 : ℚ)
+#eval  (2 - 3 : ℤ)
 
--- Define a variable.
+/- To formally define a variable, we use `def`. -/
 def a : ℕ := 2
 def b : ℕ := 3
 
--- You can use existing types to construct new types.
+/- Use existing types to construct new types. -/
+#check List
 #check List ℕ
 #check Finset ℕ
 #check Set ℕ
-#check ℕ → ℕ
+#check ℕ → ℝ
+#check ℝ × ℝ
 
 /- Some types are representable, then you can show them. But some are not. -/
-def finset_two : Finset ℕ := {1, 2}
-#eval finset_two
+def finset_A : Finset ℕ := {1, 2}
+def finset_B : Finset ℕ := {2, 3}
+def set_A : Set ℕ := {1, 2}
+def set_B : Set ℕ := {2, 3}
+#check set_A ∪ set_B
+#eval finset_A ∪ finset_B
 def list_two : List ℕ := [1, 2]
 #eval list_two
-def set_two : Set ℕ := {1, 2}
 -- #eval set_two -- This will not work.
 
-/- Construct the function type using existing types -/
-def g : ℕ → ℕ :=
-  fun x => x + 3
+/- Define a function on natural number, it can also be interpreted
+as a series. -/
+def ai : ℕ → ℕ :=
+  fun x => 1 / (x + 3)
 
 /- A more common way is to use `()` to specify the parameter -/
 def f (x : ℕ) : ℕ :=
   x + 3
 
-/- You can also define a function with other types -/
+/- You can also define a function on real line -/
 def f_real (x : ℝ) :=
   x + 3
 
 #check f
-#eval f 3
-#check g
 #check f_real
 
-
-/- The most important type in Lean is the Proposition type, which constructs theorems.
+/-
+## Propositions and Proofs
+The most important type in Lean is the Proposition type, which constructs theorems.
 A proof of a proposition is a value of that type. -/
 
 /- This is a very simple proposition. -/
 def easy_prop : Prop :=
   2 + 2 = 4
 
-/- Of course we can define more complicated -/
-
-/- These are proofs of propositions. -/
-theorem easy : 2 + 2 = 4 :=
-  rfl
-
-/-
-Note that the type of `easy` is `2 + 2 = 4`. Its value is `rfl`, which is a proof of the proposition.
--/
-#check easy
+/- We can use quantifiers to define more -/
+def medium_prop : Prop :=
+  ∀ x : ℝ, -5 < x ^ 3 ∧ x ^ 3 < 5
 
 /- The classical definitiion of limit in `ε-δ` language. -/
-def epsilon_delta_limit (f : ℝ → ℝ) (l : ℝ) (a : ℝ) :=
+def epsilon_delta_limit (f : ℝ → ℝ) (l : ℝ) (a : ℝ) : Prop :=
   ∀ ε > 0, ∃ δ > 0, ∀ x, 0 < |x - a| ∧ |x - a| < δ → |f x - l| < ε
 
 #check epsilon_delta_limit
 
-/- This theorem shows the limit of the function `f_real` at `3` is `6`
-We will not prove it here. -/
-theorem epsilon_delta_limit_of_f_real : epsilon_delta_limit f_real 3 6 := sorry
+/- These are proofs of propositions. -/
+theorem easy : easy_prop := rfl
+
+/- Note that the type of `easy` is `2 + 2 = 4`. Its value is `rfl`,
+which is a proof of the proposition. In general, if you provide a value
+of type `p`, then you prove `p` -/
+#check easy_prop
+#check easy
+
+
+
+/- Construct more complex object using proposition -/
+def set_test : Set ℝ := {x | -5 < x ^ 3 ∧ x ^ 3 < 5}
+
+/-
+## Construct a mathematical statement
+Use `def` to define an expression, whose type is a proposition.
+This theorem shows the limit of the function `f_real` at `3` is `6`
+-/
+def epsilon_delta_limit_of_f_real :
+  epsilon_delta_limit f_real 3 6 := sorry
 
 #check epsilon_delta_limit_of_f_real
 
-def FermatLastTheorem' :=
-  ∀ x y z n : ℕ, n > 2 ∧ x * y * z ≠ 0 → x ^ n + y ^ n ≠ z ^ n
+/- Moreover, you can put variables and propositions as params -/
+def epsilon_delta_limit_of_f_real' (f : ℝ → ℝ) (a : ℝ) :
+  ∃ l, ∀ ε > 0, ∃ δ > 0, ∀ x,
+  0 < |x - a| ∧ |x - a| < δ → |f x - l| < ε := sorry
 
-#check FermatLastTheorem'
-
-theorem hard : FermatLastTheorem' :=
-  sorry
-
-#check hard
-
+#check epsilon_delta_limit_of_f_real'
 
 /- Here are some proofs from MIL -/
 example : ∀ m n : Nat, Even n → Even (m * n) := fun m n ⟨k, (hk : n = k + k)⟩ ↦
@@ -126,9 +144,13 @@ example : ∀ m n : Nat, Even n → Even (m * n) := by
 example : ∀ m n : Nat, Even n → Even (m * n) := by
   intros; simp [*, parity_simps]
 
-/-- 2024 Gaokao Xinkebiao I - 1: Suppose $A= {x | -5 < x^3 < 5}$, $B = {-3, -1, 0, 2, 3}$, find $A U B. Answer is ${-1, 0}$
-已知集合 $A= {x | -5 < x^3 < 5}$, $B = {-3, -1, 0, 2, 3}$，求 $A U B$，答案为 ${-1, 0}$-/
-theorem set_example {A B : Set ℝ} (h₁ : A = {x | -5 < x ^ 3 ∧ x ^ 3 < 5})
+
+/--
+## Formalize a statement
+2024 Gaokao Xinkebiao I - 1:
+Suppose $A= {x | -5 < x^3 < 5}$, $B = {-3, -1, 0, 2, 3}$, find $A U B.
+Answer is ${-1, 0}$ -/
+def set_example (A B : Set ℝ) (h₁ : A = {x | -5 < x ^ 3 ∧ x ^ 3 < 5})
   (h₂ : B = {-3, -1, 0, 2, 3}) : A ∩ B = {-1, 0} := by
   -- To prove equality of sets is to prove that they have the same elements.
   ext x
@@ -145,4 +167,9 @@ theorem set_example {A B : Set ℝ} (h₁ : A = {x | -5 < x ^ 3 ∧ x ^ 3 < 5})
   cases' h with h h
   any_goals {simp [h]; repeat constructor; repeat linarith}
 
--- #herald "Suppose $A= {x | -5 < x^3 < 5}$, $B = {-3, -1, 0, 2, 3}$, find $A U B. Answer is ${-1, 0}"
+
+
+
+/- 2024 Gaokao Xinkebiao I - 4
+Given cos(α+β)=m, tan(α)tan(β)=2, show that cos(α-β)=-3m -/
+#herald "Given cos(α+β)=m, tan(α)tan(β)=2, show that cos(α-β)=-3m"
