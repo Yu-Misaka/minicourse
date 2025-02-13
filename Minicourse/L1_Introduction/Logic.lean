bc326b7064fa053f92d1f54394b9243611d50452
slashbade <slashbladevitali@gmail.com>
2025-02-13 23:15:28 +0800

    update L1 logic part

--- Minicourse/L1_Introduction/Logic.lean
+++ Minicourse/L1_Introduction/Logic.lean
@@ -1,5 +1,6 @@
 /-
-In this section, we will learn how to prove
+In this section, we will learn how to prove theorem using some Basic
+logic structure, starting from logical connectors and quantifiers.
 -/
 import Mathlib.Analysis.Normed.Field.Basic
 import Mathlib.Data.Real.StarOrdered
@@ -11,6 +12,72 @@ def epsilon_delta_limit (f : ℝ → ℝ) (l : ℝ) (a : ℝ) : Prop :=
 /- Infinite version of limit in `ε-δ` language -/
 def epsilon_delta_limit_inf (f : ℝ → ℝ) (l : ℝ) : Prop :=
   ∀ ε > 0, ∃ δ > 0, ∀ x > δ, |f x - l| < ε
+
+/- 
+## Deal with connectors
+
+- `x ∧ y` in goal: constructor or directly give ⟨x, y⟩
+- `h : h₁ ∧ h₂` in the context: `h.left` to obtain h₁, `h.right` to obtain
+`h₂`
+- `x ∨ y` in goal: `left` or `right`
+- `h : x ∨ y` in context: `cases' h with h₁ h₂`
+-/
+
+
+/- 
+## Deal with Universal Quatifier
+
+### Usage of `intro` tactic
+1. Implications `A → B`
+2. Universal Quantifiers `∀ x, p x`
+3. Deal with sets
+-/
+example (ε : ℝ) : ε > 0 → 1 / ε > 0 := by
+  intro hε
+  rw [gt_iff_lt] at hε ⊢ -- `rw` acts on `hε` and current goal, type \|- 
+  rw [one_div_pos]
+  exact hε
+
+example : ∀ x : ℝ, x > 0 → |x| = x := by
+  intro x hx
+  rw [abs_of_pos hx]
+
+example (s : Set α) : s ⊆ s := by
+  intro x xs
+  exact xs
+
+/- Notice that to prove this theorem, we are actually constructing a 
+function type. -/
+example (ε : ℝ) : ε > 0 → 1 / ε > 0 := fun hε => by
+  rw [gt_iff_lt] at hε ⊢
+  rw [one_div_pos]
+  exact hε
+
+/- This really shortens the proof! -/
+example : ∀ x : ℝ, x > 0 → |x| = x := fun _ hx => abs_of_pos hx
+
+/- 
+## Deal with Existence quantifier 
+
+Use `use y` tactic if you want to prove a prop of type `∃ x, ...`;
+Use `rcases h with ⟨x, hx⟩` if you want to extract information from
+`h : ∃ x, ...`
+-/
+
+example : ∃ δ : ℝ, δ < 1 := by
+  use 0
+  exact zero_lt_one
+
+example : ∃ δ : ℝ, δ < 1 := ⟨0, zero_lt_one⟩
+
+example (ε : ℝ) (hδ : ∃ δ > 0, δ < ε) : ∃ δ > 0, δ < ε / 2 := by
+  rcases hδ with ⟨δ, hδ⟩
+  use δ / 2
+  constructor
+  · simp [hδ.left]
+  rw [div_lt_div_iff_of_pos_right]
+  · exact hδ.right
+  simp
 
 /- The limit of `1/x` as `x` approaches ∞ is `0`. -/
 theorem limit_of_one_div_x_eq_zero :
@@ -48,7 +115,7 @@ theorem limit_of_one_div_x_eq_zero :
     _ = 1 / x := by
       rw [abs_of_pos]
       simp [x_pos]
-   _ < ε := by
+    _ < ε := by
       -- `gt_iff_lt` means `a > b` is equivalent to `b < a`,
       -- you will need this very often.
       rw [gt_iff_lt] at hx
