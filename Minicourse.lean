5503b56aac8f355c3e7d24ee4835c51218dda642
slashbade <slashbladevitali@gmail.com>
2025-02-14 00:05:26 +0800

    Try to add some exercises

--- Minicourse.lean
+++ Minicourse.lean
@@ -1,3 +1,3 @@
 -- This module serves as the root of the `LeanMinicourse` library.
 -- Import modules here that should be built as part of the library.
-import Minicourse.L1_Introduction.Syntax
+import Minicourse.C01_Introduction.L01_Syntax
