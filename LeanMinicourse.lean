8d8f5b9454ec4f6a2d45dba9c5f97f07ef13bbe9
slashbade <slashbladevitali@gmail.com>
2025-02-13 12:57:55 +0800

    update L2

--- LeanMinicourse.lean
+++
@@ -1,3 +0,0 @@
--- This module serves as the root of the `LeanMinicourse` library.
--- Import modules here that should be built as part of the library.
-import LeanMinicourse.L1_Introduction.Syntax
--- lakefile.lean
+++ lakefile.lean
@@ -13,5 +13,5 @@ require reaper_tac from git "https://github.com/slashbade/reaper_tac.git"
 -- require Paperproof from git "https://github.com/Paper-Proof/paperproof.git"@"main"/"lean"
 -- require reaper_tac from git "../reaper_tac"
 @[default_target]
-lean_lib «LeanMinicourse» where
+lean_lib «Minicourse» where
   -- add any library configuration options here
