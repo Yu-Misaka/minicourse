76de420a23d55c63ed8da8e91cdb6cb704303e9e
slashblade <slashbladevitali@gmail.com>
2025-03-21 09:21:52 +0800

    update new ex

--- Minicourse/C02_Tactics/Exercise.lean
+++ Minicourse/C02_Tactics/Exercise.lean
@@ -11,3 +11,31 @@ then `a ≥ e`. -/
 
 /- Let `f(x) = x ^ 2 - 2 * x + a`, `g(x) = e ^ x - x - 1`, show that `g(x) < f(x)`
 for all positive `x`. -/
+
+/- **Remark**: The following problems are fundamental in analysis. If you finished
+any of them, it will be a great work, and you are encouraged to merge your work 
+into our `Calculus Game` project. -/
+
+/- Use the limit definition from Lecture 1, define derivative of function and show
+your definition is compatible with definition in Mathlib4, literally `deriv` or
+`fderiv`. You will need this if you want to finish problems below. -/
+#check deriv
+
+/- Use the limit definition from Lecture 1, state and prove
+Fermat's lemma (about stationery point). -/
+
+/- Use the limit definition from Lecture 1, state and prove Lagrange's mean
+value theorem. -/
+
+/- Use the limit definition from Lecture 1, state and prove L'Hopital's rule for
+finite limit in both numinator and demumintaor. -/
+
+/- Use the limit definition from Lecture 1, state and prove any Cauchy's sequence
+is convergent in ℝ. -/
+
+/- Use the limit definition from Lecture 1, prove Heine-Cantor theorem.
+Let `a` be a real number, with function `f` has limit `L` at `a`. Then for every 
+sequence {a n}, if `lim n → ∞, a n = a`, we have `lim n → ∞, f(a n) = L`-/
+
+/- Use the limit definition from Lecture 1, state and prove Bolzano–Weierstrass 
+theorem. -/
