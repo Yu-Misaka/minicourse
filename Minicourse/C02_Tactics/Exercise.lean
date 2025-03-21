115c4e4dcea50df85d4c6e34fb9f67a4956dae3b
slashblade <slashbladevitali@gmail.com>
2025-03-21 11:02:28 +0800

    update ex distrib

--- Minicourse/C02_Tactics/Exercise.lean
+++ Minicourse/C02_Tactics/Exercise.lean
@@ -1,41 +1,52 @@
 import Mathlib
 
-/- Let `a`, `b`, `c` be positive real numbers satisfying `a + b + c = 1`, show that
+/- Problem 1
+Let `a`, `b`, `c` be positive real numbers satisfying `a + b + c = 1`, show that
 `a^2 / b + b^2 / c + c^2 / a ≥ 1`. -/
 
-/- Let `a`, `b`, `c` be positive real number satisfying `a + b + c = 3`, show that 
-`1 / (a ^ 3 * (b + c)) + 1 / (b ^ 3 * (a + c))1 / (c ^ 3 * (a + b)) ≥ 3 / 2`. -/
+/- Problem 2
+Let `a`, `b`, `c` be positive real number satisfying `a + b + c = 3`, show that 
+`1 / (a ^ 3 * (b + c)) + 1 / (b ^ 3 * (a + c)) + 1 / (c ^ 3 * (a + b)) ≥ 3 / 2`. -/
 
-/- Given `f(x) = e ^ x - a * x - b`, show that if `f(x) ≥ 0` for all `x`,
+/- Problem 3
+Given `f(x) = e ^ x - a * x - b`, show that if `f(x) ≥ 0` for all `x`,
 then `a ≥ e`. -/
 
-/- Let `f(x) = x ^ 2 - 2 * x + a`, `g(x) = e ^ x - x - 1`, show that `g(x) < f(x)`
+/- Problem 4
+Let `f(x) = x ^ 2 - 2 * x + a`, `g(x) = e ^ x - x - 1`, show that `g(x) < f(x)`
 for all positive `x`. -/
 
 /- **Remark**: The following problems are fundamental in analysis. If you finished
 any of them, it will be a great work, and you are encouraged to merge your work 
 into our `Calculus Game` project. -/
 
-/- Use the limit definition from Lecture 1, define derivative of function and show
+/- Problem 5 - Zhu Yelai
+Use the limit definition from Lecture 1, define derivative of function and show
 your definition is compatible with definition in Mathlib4, literally `deriv` or
 `fderiv`. You will need this if you want to finish problems below. -/
 #check deriv
 
-/- Use the limit definition from Lecture 1, state and prove
+/- Problem 6
+Use the limit definition from Lecture 1, state and prove
 Fermat's lemma (about stationery point). -/
 
-/- Use the limit definition from Lecture 1, state and prove Lagrange's mean
+/- Problem 7 - Zhang Yirui
+Use the limit definition from Lecture 1, state and prove Lagrange's mean
 value theorem. -/
 
-/- Use the limit definition from Lecture 1, state and prove L'Hopital's rule for
+/- Problem 8
+Use the limit definition from Lecture 1, state and prove L'Hopital's rule for
 finite limit in both numinator and demumintaor. -/
 
-/- Use the limit definition from Lecture 1, state and prove any Cauchy's sequence
+/- Problem 9
+Use the limit definition from Lecture 1, state and prove any Cauchy's sequence
 is convergent in ℝ. -/
 
-/- Use the limit definition from Lecture 1, prove Heine-Cantor theorem.
+/- Problem 10
+Use the limit definition from Lecture 1, prove Heine-Cantor theorem.
 Let `a` be a real number, with function `f` has limit `L` at `a`. Then for every 
 sequence {a n}, if `lim n → ∞, a n = a`, we have `lim n → ∞, f(a n) = L`-/
 
-/- Use the limit definition from Lecture 1, state and prove Bolzano–Weierstrass 
+/- Problem 11
+Use the limit definition from Lecture 1, state and prove Bolzano–Weierstrass 
 theorem. -/
